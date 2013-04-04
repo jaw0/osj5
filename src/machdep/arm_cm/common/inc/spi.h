488b3a2cad3e5638f0aa0c0c1160a9587755b74a
Jeff Weisberg <jaw@tcp4me.com>
2013-04-03 20:29:26 -0400

    spi_msg

--- src/machdep/arm_cm/common/inc/spi.h
+++ src/machdep/arm_cm/common/inc/spi.h
@@ -9,9 +9,6 @@
 #define __spi_h__
 
 
-#define SPI_XFER_TIMEOUT	-1
-#define SPI_XFER_OK		0
-
 
 #define SPI_FLAG_READ	0x1
 #define SPI_FLAG_CRC7	0x2
@@ -25,8 +22,32 @@ struct SPIConf {
 };
 
 
+
+typedef struct spi_msg {
+    int		mode;
+# define SPIMO_READ	1
+# define SPIMO_WRITE	2
+# define SPIMO_UNTIL	3
+# define SPIMO_DELAY	4
+# define SPIMO_FINISH	5	// wait for last byte to finish
+# define SPIMO_PINSOFF	6
+# define SPIMO_PINSON	7
+    int		timeout;
+    int		dlen;
+    char	*data;
+    int		response;
+    int		(*until)(int);
+
+    int		dpos;
+    utime_t	expiration;
+} spi_msg;
+
 int spi_write1(const struct SPIConf *, int);
 int spi_xfer(const struct SPIConf *, int, char *, int);
+int spi_xfer2(const struct SPIConf *, int, spi_msg*);
 
+#define SPI_XFER_OK		0
+#define SPI_XFER_TIMEOUT	-1
+#define SPI_XFER_ERROR		-2
 
 #endif /* __spi_h__ */
