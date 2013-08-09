e59076fb34f0acec4eb5a47840e12aeed0d10b86
Jeff Weisberg <jaw@tcp4me.com>
2013-08-09 00:18:16 -0400

    conf clocks

--- src/machdep/arm_cm/stm32f4/inc/clock.h
+++ src/machdep/arm_cm/stm32f4/inc/clock.h
@@ -1,6 +1,27 @@
 
+#define HSICLOCK	16000000
 
-#define SYSCLOCK	168000000
-#define APB2CLOCK	84000000
-#define APB1CLOCK	42000000
+#ifndef SYSCLOCK
+#  ifdef HSECLOCK
+#    define SYSCLOCK HSECLOCK
+#  else
+#    define SYSCLOCK HSICLOCK
+#  endif
+#endif
+
+
+#define AHBMAX   	168000000
+#define APB2MAX  	 84000000
+#define APB1MAX  	 42000000
+#define ADCBMAX  	 36000000       /* NB: depends on voltage */
+#define USBFREQ  	 48000000
+#define FLASHMAX 	 30000000       /* NB: depends on voltage */
+#define PLLVCOTARGET   	  2000000
+#define PLLVCOMIN      	192000000
+#define PLLVCOMAX      	432000000
+
+
+extern int apb1_clock_freq(), apb2_clock_freq();
+#define APB2CLOCK	apb2_clock_freq()
+#define APB1CLOCK	apb1_clock_freq()
 
--- src/machdep/arm_cm/stm32f4/src/stm32f4_init.c
+++ src/machdep/arm_cm/stm32f4/src/stm32f4_init.c
@@ -18,45 +18,163 @@
 #define R_FLASHKB	((unsigned short *)(0x1FFF7A22))
 #define R_UNIQUE	((unsigned long*)(0x1FFF7A10))
 
+#ifdef HSECLOCK
+#  define SRCCLOCK	HSECLOCK
+#else
+#  define SRCCLOCK	HSICLOCK
+#endif
+
 
 unsigned long bootflags = 0;
 void stm32_putchar(int);
 
-int
-sys_clock_freq(void){
-    return SYSCLOCK;
-}
+int freq_sys=HSICLOCK, freq_apb1=HSICLOCK, freq_apb2=HSICLOCK;
+
+int sys_clock_freq(void){  return freq_sys;  }
+int apb1_clock_freq(void){ return freq_apb1; }
+int apb2_clock_freq(void){ return freq_apb2; }
+
+
+// fvco = hse * plln / pllm
+// fsys = fvco / pllp
+// fusb = fvco / pllq = 48MHz
+
+// hse / pllm must be 1-2MHz, 2=recommended
+// fvco must be 192-432 MHz
+// m - 6bits, n - 9 bits, q - 4 bits, p - 2 bits (2,4,6,8)
 
 static void
 clock_init(void){
+    int clksrc = 0;
+
+#if !defined(HSECLOCK) && (SYSCLOCK == HSICLOCK)
+    // run on hsi, asis
+    return;
+#endif
+
+#ifdef HSECLOCK
+#  if (HSECLOCK < 4000000) || (HSECLOCK > 26000000)
+#    error "not a valid HSE frequency. must be: 4MHz - 26MHz"
+#  endif
 
     /* enable HSE */
     RCC->CR |= (1<<16);
     while( RCC->CR & (1<<17) == 0 ){} /* wait for it */
+    clksrc   = 1;
+    freq_sys = HSECLOCK;
+#endif
 
-    FLASH->ACR |= 5;            /* flash mem wait states */
+    /****************************************************************/
 
-    RCC->PLLCFGR = (1<<22)      // src=hse
-        | 6                     // pllm
-        | (168 << 6)            // plln
-        | (0<<16)               // pllp=2
-        | (7<<24)               // pllq
-        ;
+    /* flash wait states */
+    if( SYSCLOCK > FLASHMAX ){
+        int fws = (SYSCLOCK + FLASHMAX - 1) / FLASHMAX - 1;
 
-    RCC->CFGR  = (4<<13)        // apb2 = divide by 2
-        | (5<<10)               // apb1 = divide by 4
-        | (0<<4)                // ahb =  no divide
-        | (12<<16)              // rtc = hse / 12
-        ;
+        FLASH->ACR &= 7;
+        FLASH->ACR |= fws;
+    }
 
-    RCC->CR    |= (1<<24);               /* enable pll */
+    /****************************************************************/
+    /* config PLL */
+
+#if SYSCLOCK != SRCCLOCK
+
+    clksrc = 2;
+#  ifdef HSECLOCK
+#    define PLLSRC	1
+#  else
+#    define PLLSRC 	0
+#endif
+
+#  define PLLM ((SRCCLOCK + PLLVCOTARGET - 1) / PLLVCOTARGET)
+#  if (PLLM < 2) || (PLLM > 63)
+#    error "cannot configure pll. hseclock should be 4MHz - 26MHz"
+#  endif
+
+#  define PLLFVIN	(SRCCLOCK / PLLM)
+
+#  if   (PLLVCOMIN <= SYSCLOCK * 2) && (PLLVCOMAX >= SYSCLOCK * 2)
+#    define PLLP	2
+#    define PLLPB	0
+#  elif (PLLVCOMIN <= SYSCLOCK * 4) && (PLLVCOMAX >= SYSCLOCK * 4)
+#    define PLLP	4
+#    define PLLPB	1
+#  elif (PLLVCOMIN <= SYSCLOCK * 6) && (PLLVCOMAX >= SYSCLOCK * 6)
+#    define PLLP	6
+#    define PLLPB	2
+#  elif (PLLVCOMIN <= SYSCLOCK * 8) && (PLLVCOMAX >= SYSCLOCK * 8)
+#    define PLLP	8
+#    define PLLPB	3
+#  else
+#    error "cannot configure requested sysclock. should be 24MHz - 216MHz"
+#  endif
+
+#  define PLLN		((SYSCLOCK * PLLP + PLLFVIN/2) / PLLFVIN)
+#  define PLLFVCO 	(PLLFVIN * PLLN)
+#  define PLLQ		((PLLFVCO + USBFREQ/2) / USBFREQ)
+
+        freq_sys = PLLFVCO / PLLP;
+
+        //bootmsg("pll: m %d, n %d, p %d, q %d; vco %d, sys: %d, usb %d\n", PLLM, PLLN, PLLP, PLLQ,
+        //        PLLFVCO, freq_sys, PLLFVCO/PLLQ);
+
+        RCC->PLLCFGR = (PLLSRC << 22)  // src
+            | PLLM
+            | (PLLN  << 6)
+            | (PLLPB << 16)
+            | (PLLQ  << 24)
+            ;
+
+    RCC->CR |= (1<<24);               /* enable pll */
     while( RCC->CR & (1<<25) == 0 ){} /* wait for it */
+#endif
 
-    /* Set SYSCLK as PLL */
-    RCC->CFGR |= 0x2;
-    while( RCC->CFGR & 0xC == 0 ){} /* wait for it */
+    /****************************************************************/
+
+    /* config bus clocks */
+
+    int cfgr =
+#  if (SYSCLOCK <= APB1MAX)
+        (0);			// all clocks not divided
+    freq_apb1 = freq_apb2 = freq_sys;
+#  elif (SYSCLOCK <= APB2MAX)
+        (0<<4)              	// ahb =  no divide
+        | (0<<13)      		// apb2 = no divide
+        | (4<<10);              // apb1 = divide by 2
+        freq_apb2 = freq_sys;
+        freq_apb1 = freq_sys / 2;
+#  elif (SYSCLOCK <= AHBMAX)
+        (0<<4)              	// ahb =  no divide
+        | (4<<13)      		// apb2 = divide by 2
+        | (5<<10);              // apb1 = divide by 4
+        freq_apb2 = freq_sys / 2;
+        freq_apb1 = freq_sys / 4;
+#  else
+        // unsupported - overclocking
+        (8<<4)              	// ahb =  divide by 2
+        | (5<<13)      		// apb2 = divide by 4
+        | (6<<10);              // apb1 = divide by 8
+        freq_apb2 = freq_sys / 4;
+        freq_apb1 = freq_sys / 8;
+#  endif
+
+
+#  ifdef HSECLOCK
+        cfgr  |= ((HSECLOCK / 1000000) << 16);	// rtc = hse / N
+#  endif
+
+    RCC->CFGR = cfgr;
+
+    /****************************************************************/
+
+    /* set sysclock */
+    if( clksrc ){
+        RCC->CFGR |= clksrc;
+        while( RCC->CFGR & 0xC == 0 ){} /* wait for it */
+    }
 
 }
+
 
 /****************************************************************/
 void
@@ -86,9 +204,8 @@ init_hw2(void){
 
     int ram = (&_estack - &_sdata)/1024;
     bootmsg("bootflags = 0x%x, cpuid %x, %dk flash, %dk RAM\n", bootflags, SCB->CPUID, *R_FLASHKB, ram);
+    bootmsg("clocks: sys %dMHz, apb1 %dMHz, apb2 %dMHz\n", freq_sys/1000000, freq_apb1/1000000, freq_apb2/1000000);
     bootmsg("uid %x-%x-%x\n", R_UNIQUE[0], R_UNIQUE[1], R_UNIQUE[2]);
-
-    // RSN - uid, devid, mem, ...
 
 }
 
