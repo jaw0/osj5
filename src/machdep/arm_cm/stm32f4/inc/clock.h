7405e8a097614a111029b5102d044f27fede2508
Jeff Weisberg <jaw@tcp4me.com>
2015-03-28 12:44:42 -0400

    stm32f411 max speeds

--- src/machdep/arm_cm/stm32f4/inc/clock.h
+++ src/machdep/arm_cm/stm32f4/inc/clock.h
@@ -14,16 +14,27 @@
 #endif
 
 
-#define AHBMAX   	168000000
-#define APB2MAX  	 84000000
-#define APB1MAX  	 42000000
-#define ADCBMAX  	 36000000       /* NB: depends on voltage */
-#define USBFREQ  	 48000000
-#define FLASHMAX 	 30000000       /* NB: depends on voltage */
-#define PLLVCOTARGET   	  2000000
-#define PLLVCOMIN      	192000000
-#define PLLVCOMAX      	432000000
-
+#ifdef STM32F411
+#  define AHBMAX   	100000000	/* depends on voltage, and VOS bits */
+#  define APB2MAX  	100000000	/* depends on voltage, and VOS bits */
+#  define APB1MAX  	 50000000	/* depends on voltage, and VOS bits */
+#  define ADCBMAX  	 36000000       /* NB: depends on voltage */
+#  define USBFREQ  	 48000000
+#  define FLASHMAX 	 30000000       /* NB: depends on voltage */
+#  define PLLVCOTARGET    2000000
+#  define PLLVCOMIN     192000000
+#  define PLLVCOMAX     432000000
+#else
+#  define AHBMAX   	168000000
+#  define APB2MAX  	 84000000
+#  define APB1MAX  	 42000000
+#  define ADCBMAX  	 36000000       /* NB: depends on voltage */
+#  define USBFREQ  	 48000000
+#  define FLASHMAX 	 30000000       /* NB: depends on voltage */
+#  define PLLVCOTARGET    2000000
+#  define PLLVCOMIN     192000000
+#  define PLLVCOMAX     432000000
+#endif
 
 extern int sys_clock_freq(), apb1_clock_freq(), apb2_clock_freq();
 #define APB2CLOCK	apb2_clock_freq()
