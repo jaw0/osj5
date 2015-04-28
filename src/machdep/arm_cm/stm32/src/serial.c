aba82b0c32fd7eee9863d49991432aed15a1c20f
Jeff Weisberg <jaw@tcp4me.com>
2015-04-28 00:41:46 -0400

    serial baud

--- src/machdep/arm_cm/stm32/src/serial.c
+++ src/machdep/arm_cm/stm32/src/serial.c
@@ -57,7 +57,7 @@ struct Com {
     u_char head, tail, len;
 
     u_char status;
-    u_char bauddiv;
+    int    baudclock;
 #define COMSTAT_THROTTLED	2	/* we have dropped RTS */
 
     int portno;
@@ -84,7 +84,6 @@ serial_init(struct Device_Conf *dev){
     com[i].head = com[i].tail = com[i].len = 0;
     com[i].status  = 0;
     com[i].portno  = i;
-    com[i].bauddiv = 1;
     com[i].file.d  = (void*)&com[i];
 
     // enable gpio clock, usart clock, configure pins
@@ -94,6 +93,7 @@ serial_init(struct Device_Conf *dev){
         addr = USART1;
         RCC->APB2ENR |= 0x4005;		// usart1+gpioA+afi
         GPIOA->CRH   |= 0x4b0;
+        com[i].baudclock = apb2_clock_freq();
         irq           = (int) IRQ_USART1;
         break;
     case 1:
@@ -101,7 +101,7 @@ serial_init(struct Device_Conf *dev){
         RCC->APB2ENR |= 0x5;
         RCC->APB1ENR |= 0x20000;
         GPIOA->CRL    = 0x4b00;
-        com[i].bauddiv = 2;
+        com[i].baudclock = abp1_clock_freq();
         irq           = (int) IRQ_USART2;
         break;
     case 2:
@@ -109,7 +109,7 @@ serial_init(struct Device_Conf *dev){
         RCC->APB2ENR |= 0x9;
         RCC->APB1ENR |= 0x40000;
         GPIOB->CRH    = 0x4b00;
-        com[i].bauddiv = 2;
+        com[i].baudclock = apb1_clock_freq();
         irq           = (int) IRQ_USART3;
         break;
     }
@@ -122,11 +122,11 @@ serial_init(struct Device_Conf *dev){
             gpio_init( GPIO_B6, GPIO_AF(7) | GPIO_PUSH_PULL );
             gpio_init( GPIO_B7, GPIO_AF(7) | GPIO_PUSH_PULL );
         }else{
-            gpio_init( GPIO_A9, GPIO_AF(7) | GPIO_PUSH_PULL );
+            gpio_init( GPIO_A9,  GPIO_AF(7) | GPIO_PUSH_PULL );
             gpio_init( GPIO_A10, GPIO_AF(7) | GPIO_PUSH_PULL );
         }
         RCC->APB2ENR |= 1<<4;
-        com[i].bauddiv = 2;
+        com[i].baudclock = apb2_clock_freq();
         break;
     case 1:
         addr = USART2;
@@ -139,7 +139,7 @@ serial_init(struct Device_Conf *dev){
             gpio_init( GPIO_A3, GPIO_AF(7) | GPIO_PUSH_PULL );
         }
         RCC->APB1ENR |= 1<<17;
-        com[i].bauddiv = 4;
+        com[i].baudclock = apb1_clock_freq();
         break;
     case 2:
         addr = USART3;
@@ -154,7 +154,7 @@ serial_init(struct Device_Conf *dev){
         }
         // alt: d8, d9
         RCC->APB1ENR |= 1<<18;
-        com[i].bauddiv = 4;
+        com[i].baudclock = apb1_clock_freq();
         break;
     case 3:
         addr = UART4;
@@ -167,7 +167,7 @@ serial_init(struct Device_Conf *dev){
             gpio_init( GPIO_A1, GPIO_AF(8) | GPIO_PUSH_PULL );
         }
         RCC->APB1ENR |= 1<<19;
-        com[i].bauddiv = 4;
+        com[i].baudclock = apb1_clock_freq();
         break;
 
         // case 4:
@@ -204,7 +204,7 @@ serial_setbaud(int port, int baud){
     int d, i;
     int plx;
 
-    com[port].addr->BRR = sys_clock_freq() / baud / com[port].bauddiv;;
+    com[port].addr->BRR = com[port].baudclock / baud;
 }
 
 int
