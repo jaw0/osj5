7af2fb55ed1775731d1a7c02e2f6830344c49233
Jeff Weisberg <jaw@tcp4me.com>
2018-03-10 14:27:46 -0500

    missing file

---
+++ src/machdep/arm_cm/stm32f7/inc/clock.h
@@ -0,0 +1,47 @@
+
+#ifndef __stm32f7_clock_h__
+#define __stm32f7_clock_h__
+
+
+#define HSICLOCK	16000000
+
+#ifndef SYSCLOCK
+#  ifdef HSECLOCK
+#    define SYSCLOCK HSECLOCK
+#  else
+#    define SYSCLOCK HSICLOCK
+#  endif
+#endif
+
+
+#ifdef STM32F7_OVERDRIVE
+#  define SYSMAX	216000000
+#  define AHBMAX   	216000000
+#  define APB2MAX  	108000000
+#  define APB1MAX  	 54000000
+#  define ADCBMAX  	 36000000       /* NB: depends on voltage */
+#  define USBFREQ  	 48000000
+#  define FLASHMAX 	 30000000       /* NB: depends on voltage */
+#  define PLLVCOTARGET    2000000
+#  define PLLVCOMIN     100000000
+#  define PLLVCOMAX     432000000
+
+#else
+#  define SYSMAX	216000000
+#  define AHBMAX   	180000000	/* with over-drive not enabled */
+#  define APB2MAX  	 90000000	/* "" */
+#  define APB1MAX  	 45000000	/* "" */
+#  define ADCBMAX  	 36000000       /* NB: depends on voltage */
+#  define USBFREQ  	 48000000
+#  define FLASHMAX 	 30000000       /* NB: depends on voltage */
+#  define PLLVCOTARGET    2000000
+#  define PLLVCOMIN     100000000
+#  define PLLVCOMAX     432000000
+#endif
+
+extern int sys_clock_freq(), apb1_clock_freq(), apb2_clock_freq();
+#define APB2CLOCK	apb2_clock_freq()
+#define APB1CLOCK	apb1_clock_freq()
+
+
+#endif /* __stm32f7_clock_h__ */
